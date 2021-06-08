038cfb502b1538d941966de49f753342d60ad90b
yojimat <visoccer22@hotmail.com>
2021-06-07 23:32:32 -0300

    adicionado about_true_and_false e triangle para about_triangle_project

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b and c == b
+    return :equilateral
+  end
+  if a == b or b == c or a == c
+    return :isosceles
+  end
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
